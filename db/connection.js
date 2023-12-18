a5f074806bf2028257bd83b9bd641e1c807dd456
Biswajit Ghosh <bg5050525@gmail.com>
2023-12-18 13:02:22 +0530

    adding more

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-    console.log(process.env.MONGODB_URI)
+    // console.log(process.env.MONGODB_URI)
     try {
         try{
-            let res=await mongoose.connect(process.env.MONGODB_URI);
+            let res=await mongoose.connect('mongodb+srv://biswajit2329:T1voipAip4RSgv97@cluster0.fw5wwvc.mongodb.net/e-comm?retryWrites=true&w=majority');
             console.log(`\nMongoDB connected`);
         }catch{
             console.log("we find some error")
